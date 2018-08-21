907f7de50f539ecfc4cc1b6d6c9d2381f466b640
github username <github email address>
2018-08-21 14:41:04 -0400

    serialized park_code in parks

--- app/serializers/park_serializer.rb
+++ app/serializers/park_serializer.rb
@@ -1,4 +1,4 @@
 class ParkSerializer < ActiveModel::Serializer
-  attributes :id, :api_id, :description, :designation, :full_name, :coordinates, :url, :weather_info, :image_titles, :image_sources
+  attributes :id, :api_id, :description, :designation, :full_name, :coordinates, :url, :weather_info, :image_titles, :image_sources, :park_code
   has_many :states
 end
